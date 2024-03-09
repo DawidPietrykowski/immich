1e7d2c6c628c7397c7d8a0ee8b5f57559e00b68d
DawidPietrykowski <servces+github@dawidpietrykowski.com>
2024-03-09 09:03:11 +0100

    WIP

--- mobile/lib/modules/home_widget/services/favorites_widget_service.dart
+++ mobile/lib/modules/home_widget/services/favorites_widget_service.dart
@@ -3,18 +3,22 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui';
 
+import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/constants/app_widget_data.dart';
+import 'package:immich_mobile/modules/asset_viewer/image_providers/immich_local_image_provider.dart';
 import 'package:immich_mobile/modules/favorite/providers/favorite_provider.dart';
 import 'package:immich_mobile/shared/models/asset.dart';
 import 'package:immich_mobile/shared/providers/db.provider.dart';
 import 'package:immich_mobile/shared/providers/user.provider.dart';
 import 'package:immich_mobile/shared/services/api.service.dart';
+import 'package:immich_mobile/shared/ui/immich_image.dart';
 import 'package:immich_mobile/shared/ui/immich_thumbnail.dart';
 import 'package:isar/isar.dart';
+import 'package:logging/logging.dart';
 
 final favoriteAssetsListProvider = Provider<List<Asset>>((ref) {
   final user = ref.watch(currentUserProvider);
@@ -39,25 +43,31 @@ final favoritesWidgetServiceProvider = StateProvider<FavoritesWidgetService>((re
 });
 
 class FavoritesWidgetService{
+  final log = Logger("MemoryService");
+
   List<Asset> favoriteAssets;
   final _globalKey = GlobalKey();
 
   FavoritesWidgetService(this.favoriteAssets);
 
   void updateWidget() async {
-    if (favoriteAssets.length == 0){
-      print('No favorite assets');
+    if (favoriteAssets.isEmpty){
+      log.log(Level.INFO, 'No favorite assets');
       return;
     }
     Asset selectedAsset = favoriteAssets[0];
-    print('Selected asset: ${selectedAsset.name}');
+    Widget imageWidget = ImageWidget(image: ImmichImage.imageProvider(
+      asset: selectedAsset,
+    )).build(_globalKey.currentContext!);
+    log.log(Level.INFO, 'Selected asset: ${selectedAsset.name}');
     var path = await HomeWidget.renderFlutterWidget(
-      ImageWidget(image: ImmichThumbnail.imageProvider(asset: selectedAsset),),
+      imageWidget,
       key: 'filename',
       logicalSize: _globalKey.currentContext!.size!,
       pixelRatio:
       MediaQuery.of(_globalKey.currentContext!).devicePixelRatio,
     ) as String;
+    log.log(Level.INFO, 'Widget rendered at $path');
     HomeWidget.saveWidgetData<String>('filename', path);
     HomeWidget.updateWidget(
       iOSName: iOSWidgetName,
